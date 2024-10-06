e75b80a4b5e14a9d74f6a38264580e3824457ef1
venkata trinadh <venkatatrinadh4444@gmail.com>
2024-10-06 16:51:22 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function color1(){
+    document.getElementById('colorPickerContainer').style.backgroundColor="#e0e0e0";
+    document.getElementById('selectedColorHexCode').textContent="#e0e0e0";
+}
+function color2(){
+    document.getElementById('colorPickerContainer').style.backgroundColor="#6fcf97";
+    document.getElementById('selectedColorHexCode').textContent="#6fcf97";
+}
+function color3(){
+    document.getElementById('colorPickerContainer').style.backgroundColor="#56ccf2";
+    document.getElementById('selectedColorHexCode').textContent="#56ccf2";
+}
+function color4(){
+    document.getElementById('colorPickerContainer').style.backgroundColor="#bb6bd9";
+    document.getElementById('selectedColorHexCode').textContent="#bb6bd9";
+}
